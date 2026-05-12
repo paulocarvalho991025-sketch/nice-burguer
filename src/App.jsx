246c79f3c23305ff0b18a78d7d544c846d40d9b8
Paulo Rento <paulocarvalho991025@gmail.com>
2026-05-12 20:52:21 -0300

    atualizacao do site

--- src/App.jsx
+++ src/App.jsx
@@ -382,7 +382,7 @@ ${observacao || "Nenhuma"}
 </div>
 <p
   className="mt-5 text-5xl text-black-700"
-  style={{ fontFamily: "Yellowtail" }}
+  style={{ fontFamily: "Kaushan Script" }}
 >
   O Burguer que você merece!
 </p>
