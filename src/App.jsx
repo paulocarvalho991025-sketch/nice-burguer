e8591ea6fde08051376aa6d767d6667de75e32a7
Paulo Rento <paulocarvalho991025@gmail.com>
2026-05-13 18:17:53 -0300

    atualizacao do site

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,14 @@ const [menuAberto, setMenuAberto] = useState(false);
 
 const [carrinho, setCarrinho] = useState(() => {
   const carrinhoSalvo = localStorage.getItem("carrinho");
+function adicionarCombo(combo) {
+  adicionarAoCarrinho(combo);
+  setComboAdicionado(combo.nome);
 
+  setTimeout(() => {
+    setComboAdicionado("");
+  }, 1000);
+}
   return carrinhoSalvo
     ? JSON.parse(carrinhoSalvo)
     : [];
@@ -561,14 +568,14 @@ className="rounded-[2rem] bg-gradient-to-br from-[#ff3c00] via-[#ff7b00] to-[#ff
 
 <motion.button
  type="button"
- onClick={() => adicionarAoCarrinho(combo)}
+ onClick={() => adicionarCombo(combo)}
  whileTap={{ scale: 0.9 }}
  whileHover={{ scale: 1.05 }}
  animate={{ y: [0, -2, 0] }}
  transition={{ duration: 0.3 }}
  className="absolute bottom-4 left-4 z-20 rounded-full bg-orange-600 px-4 py-2 text-sm font-black text-white shadow-xl transition hover:scale-105 hover:bg-orange-500"
 >
-  Adicionar
+  {comboAdicionado === combo.nome ? "Adicionado!" : "Adicionar"}
 </motion.button>
 
     </div>
