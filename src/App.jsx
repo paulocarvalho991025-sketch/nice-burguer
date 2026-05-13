97ca0effcf2871317c4ccf994894bed6ef3cd2c7
Paulo Rento <paulocarvalho991025@gmail.com>
2026-05-13 18:25:16 -0300

    atualizacao do site

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ const [carrinho, setCarrinho] = useState(() => {
 });
 
 const [carrinhoAberto, setCarrinhoAberto] = useState(false);
+const [comboAdicionado, setComboAdicionado] = useState("");
 const [endereco, setEndereco] = useState(() => {
   return localStorage.getItem("endereco") || "";
 });
@@ -158,6 +159,13 @@ const combos = [
 ];
     function adicionarAoCarrinho(produto) {
   setCarrinho((atual) => [...atual, produto]);
+}function adicionarCombo(combo) {
+  adicionarAoCarrinho(combo);
+  setComboAdicionado(combo.nome);
+
+  setTimeout(() => {
+    setComboAdicionado("");
+  }, 1000);
 }
 return (
   <main className="min-h-screen bg-[#f6f0e7] relative overflow-hidden">
@@ -560,15 +568,13 @@ className="rounded-[2rem] bg-gradient-to-br from-[#ff3c00] via-[#ff7b00] to-[#ff
 
 
 <motion.button
- type="button"
- onClick={() => adicionarAoCarrinho(combo)}
- whileTap={{ scale: 0.9 }}
- whileHover={{ scale: 1.05 }}
- animate={{ y: [0, -2, 0] }}
- transition={{ duration: 0.3 }}
- className="absolute bottom-4 left-4 z-20 rounded-full bg-orange-600 px-4 py-2 text-sm font-black text-white shadow-xl transition hover:scale-105 hover:bg-orange-500"
+  type="button"
+  onClick={() => adicionarCombo(combo)}
+  whileTap={{ scale: 0.9 }}
+  whileHover={{ scale: 1.05 }}
+  className="absolute bottom-4 left-4 z-20 rounded-full bg-orange-600 px-4 py-2 text-sm font-black text-white shadow-xl"
 >
-  Adicionar
+  {comboAdicionado === combo.nome ? "Adicionado!" : "Adicionar"}
 </motion.button>
 
     </div>
