5ba4f923b16456ac909abf26a8f506f8a9ea5cae
Paulo Rento <paulocarvalho991025@gmail.com>
2026-05-16 10:42:32 -0300

    atualizacao do site

--- src/App.jsx
+++ src/App.jsx
@@ -146,7 +146,7 @@ const produtos = [
     nome: "Burguer 1.0",
     descricao: "Pão Brioche, Blend Bovino 80g, Queijo Mussarela, e Molho da casa.",
     preco: "R$ 9,99",
-    imagem:"/img/1.png",
+    imagem:"/img/um.png",
   },
   {
     nome: "Burguer 2.0",
