250e54b59fede335287433b28defaca41b23e5bb
Paulo Rento <paulocarvalho991025@gmail.com>
2026-05-16 12:03:00 -0300

    atualizacao do site

--- src/App.jsx
+++ src/App.jsx
@@ -220,7 +220,7 @@ const combos = [
     nome: "Combo Street",
     descricao: "Burguer 2.0 + Batata Frita + Coca Lata 350ml",
     preco: "R$ 27,97",
-    imagem: "/img/cst.png",
+    imagem: "/img/maisquerido.png",
     
   },
   {
@@ -555,7 +555,7 @@ className="rounded-[2rem] bg-[#1f1a18] p-0 shadow-[0_0_80px_rgba(255,120,0,0.25)
       nome: "Combo Street",
       descricao: "Burguer 2.0 + Batata Frita + Coca-Cola Lata 350ml + Molho Especial",
       preco: "R$ 27,97",
-      imagem: "/img/cst.png",
+      imagem: "/img/maisquerido.png",
     })
   }
   className="mt-6 w-full rounded-2xl bg-orange-600 py-4 text-lg font-black text-white shadow-xl transition hover:bg-orange-500"
