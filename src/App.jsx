00c676efe8a3fb73239a38916aeec2d91f9c6a69
Paulo Rento <paulocarvalho991025@gmail.com>
2026-05-30 17:14:22 -0300

    atualizacao do site

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";import { ShoppingBag, MessageCircle, Star, Clock, MapPin, } from "lucide-react";
 import { motion, AnimatePresence } from "framer-motion";
+import React, { useState, useEffect } from "react";import { ShoppingBag, MessageCircle, Star, Clock, MapPin, } from "lucide-react";
 export default function App() {
 const [imagem3, setImagem3] = useState(0);useEffect(() => {
   const interval = setInterval(() => {
