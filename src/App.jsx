43941a37b6bd385c24be1c036cb45043983f4676
Paulo Rento <paulocarvalho991025@gmail.com>
2026-05-31 07:49:04 -0300

    atualizacao do site

--- src/App.jsx
+++ src/App.jsx
@@ -973,42 +973,39 @@ className="rounded-[2rem] bg-[#1f1a18] p-0 shadow-[0_0_80px_rgba(255,120,0,0.25)
 <div className="flex gap-5 overflow-x-auto scroll-smooth pb-4 md:grid md:grid-cols-3 md:overflow-visible">
   {fritas.map((produto) => (
   <motion.div
-  key={produto.nome}
-  whileHover={{ y: -3, scale: 0.98 }}
-  whileTap={{ scale: 0.98 }}
-  transition={{ duration: 0.2 }}
-  className="min-w-[300px] md:min-w-0 overflow-hidden rounded-3xl bg-[#1f1a18] p-5"
->
-  <img
-    src={produto.imagem}
-    alt={produto.nome}
-    className="h-[260px] w-full object-cover"
-  />
+    key={produto.nome}
+    whileHover={{ y: -8, scale: 1.05 }}
+    whileTap={{ scale: 0.98 }}
+    transition={{ duration: 0.2 }}
+className="min-w-[280px] md:min-w-0 relative overflow-hidden rounded-3xl bg-[#1f1a18]/10 p-5 shadow-xl backdrop-blur" >
+<div className="relative mb-4 flex h-[280px] items-center justify-center overflow-hidden rounded-2xl bg-orange-100">      
 
-  <h3 className="mt-6 text-2xl font-black text-white">
-    {produto.nome}
-  </h3>
+      <img
+        src={produto.imagem}
+        alt={produto.nome}
+className="h-full w-full object-cover"      />
+    </div>
 
-  <p className="mt-3 min-h-[90px] text-sm leading-6 text-stone-300">
-    {produto.descricao}
-  </p>
+    <h3 className="text-xl font-black text-white">
+      {produto.nome}
+    </h3>
 
-  <div className="mt-5 flex items-center justify-between">
-    <span className="text-4xl font-black text-orange-500">
-      {produto.preco}
-    </span>
+    <p className="mt-2 min-h-[70px] text-sm leading-6 text-stone-300">
+      {produto.descricao}
+    </p>
 
-    <motion.button
-      type="button"
-      whileTap={{ scale: 0.9 }}
-      whileHover={{ scale: 1.05 }}
-      onClick={() => adicionarItem(produto)}
-      className="rounded-full bg-orange-600 px-4 py-2 text-sm font-black text-white hover:bg-orange-500"
-    >
-      {comboAdicionado === produto.nome ? "Adicionado!" : "Adicionar"}
-    </motion.button>
-  </div>
-</motion.div>
+<div className="mt-6">      
+
+      <motion.button
+        type="button"
+        whileTap={{ scale: 0.9 }}
+        whileHover={{ scale: 1.05 }}
+        onClick={() => adicionarItem(produto)}
+className="w-full rounded-2xl bg-orange-600 py-4 text-lg font-black text-white shadow-xl hover:bg-orange-500"      >
+        {comboAdicionado === produto.nome ? "Adicionado à sacola!" : "Adicionar"}
+      </motion.button>
+    </div>
+  </motion.div>
 ))}
 </div>
 <div className="mb-10 mt-20 text-center">
@@ -1065,7 +1062,7 @@ className="rounded-[2rem] bg-[#1f1a18] p-0 shadow-[0_0_80px_rgba(255,120,0,0.25)
   </h2>
 </div>
 
-<div className="flex justify-center">
+<div className="flex gap-5 overflow-x-auto scroll-smooth pb-4 md:grid md:grid-cols-3 md:overflow-visible">
   {sucos.map((produto) => (
     <motion.div
   key={produto.nome}
@@ -1104,6 +1101,125 @@ className="rounded-[2rem] bg-[#1f1a18] p-0 shadow-[0_0_80px_rgba(255,120,0,0.25)
     </motion.button>
   </div>
 </motion.div>
+  ))}
+</div>
+      <section id="combos" className="bg-stone-900 py-16 text-white">
+        <div className="mx-auto max-w-6xl px-5">
+          <div className="mb-10 text-center">
+  <p className="font-bold text-orange-300 md:text-xl">
+    Em Nossa Hamburgueria Delivery
+  </p>
+
+  <h2 className="text-3xl font-black md:text-4xl">
+    Você aproveita o melhor do hamburguer artesanal com um ótimo custo beneficio!
+  </h2>
+</div>
+<div className="flex gap-5 overflow-x-auto scroll-smooth pb-4 md:grid md:grid-cols-3 md:overflow-visible">
+  {combosNormais.map((combo) => (
+  <motion.div
+  key={combo.nome}
+  whileHover={{ y: -3, scale: 0.95 }}
+  whileTap={{ scale: 0.98 }}
+  transition={{ duration: 0.2 }}
+  className="fire-card min-w-[280px] md:min-w-0 overflow-hidden rounded-3xl bg-[#1f1a18]/20 border-2 border-orange-500 shadow-[0_0_15px_rgba(255,120,0,0.6),0_0_35px_rgba(255,80,0,0.4)]"
+>  
+  
+    <img
+      src={combo.imagem}
+      alt={combo.nome}
+      className="h-[280px] w-full object-contain"
+    />
+
+  
+<div className="p-5">
+  <h3 className="text-2xl font-black text-white">
+    {combo.nome}
+  </h3>
+
+  <p className="mt-3 text-stone-300">
+    {combo.descricao}
+  </p>
+
+  <div className="mt-5 flex items-center justify-between">
+    <span className="text-3xl font-black text-orange-400">
+      {combo.preco}
+    </span>
+
+    <motion.button
+      type="button"
+      whileTap={{ scale: 0.9 }}
+      whileHover={{ scale: 1.05 }}
+      onClick={() => adicionarItem(combo)}
+      className="rounded-full bg-orange-600 px-4 py-2 text-sm font-bold text-white"
+    >
+      {comboAdicionado === combo.nome
+        ? "✓ Adicionado"
+        : "Adicionar"}
+    </motion.button>
+  </div>
+</div>
+  </motion.div>
+))}
+</div>
+</div>
+      </section>
+      <div className="mt-24 text-center">
+  <h2 className="text-4xl font-black text-white">
+    Festas & Eventos
+  </h2>
+
+  <p className="mt-4 text-xl font-bold text-white-300">
+    Já pensou a NiceBurguer na sua festa ou evento?
+  </p>
+
+  <p className="mt-2 text-lg font-black text-white-200">
+    Faça já seu agendamento!
+  </p>
+</div>
+
+<div className="mt-12 grid gap-5 md:grid-cols-3">
+  {eventos.map((produto) => (
+    <motion.div
+      key={produto.nome}
+      whileHover={{ y: -8, scale: 1.05 }}
+      whileTap={{ scale: 0.98 }}
+      transition={{ duration: 0.2 }}
+className="min-w-[280px] md:min-w-0 flex flex-col rounded-3xl bg-[#1f1a18]/80 p-5 shadow-[0_0_40px_rgba(255,120,0,0.18)] transition hover:shadow-[0_0_60px_rgba(255,120,0,0.35)]"   >
+<div className="relative mb-4 flex h-[340px] items-center justify-center overflow-hidden rounded-2xl bg-[#2b211d]">        
+
+        <img
+          src={produto.imagem}
+          alt={produto.nome}
+          className="relative z-10 h-full w-auto object-contain"
+        />
+      </div>
+
+      <h3 className="text-xl font-black text-white">
+        {produto.nome}
+      </h3>
+
+      <p className="mt-2 min-h-16 text-sm leading-6 text-stone-300">
+        {produto.descricao}
+      </p>
+
+      <div className="mt-5 flex items-center justify-between">
+        <span className="text-2xl font-black text-orange-700">
+          {produto.preco}
+        </span>
+
+        <motion.button
+          type="button"
+          whileTap={{ scale: 0.9 }}
+          whileHover={{ scale: 1.05 }}
+          onClick={() => agendar(produto)}
+          className="rounded-full bg-orange-600 px-3 py-1 text-sm font-bold text-white hover:bg-orange-700"
+        >
+          {comboAdicionado === produto.nome
+            ? "Agendado!"
+            : "Agendar"}
+        </motion.button>
+      </div>
+    </motion.div>
   ))}
 </div>
 <div className="mt-24 text-center">
